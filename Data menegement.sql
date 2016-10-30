d473ab2e4d747c8fc3dafe50a3fdd229bb0f8543
saixen <saixenia@gmail.com>
2016-10-30 17:58:23 +0300

    Вторая версия схемы

--- Data menegement.sql
+++ Data menegement.sql
@@ -1,20 +1,31 @@
+DROP TABLE Brake CASCADE
+;
 DROP TABLE Cargo CASCADE
 ;
 DROP TABLE Carriage CASCADE
 ;
 DROP TABLE Carriage retarders CASCADE
 ;
-DROP TABLE Full name CASCADE
+DROP TABLE Operation CASCADE
 ;
 DROP TABLE Operator CASCADE
 ;
 DROP TABLE Switch CASCADE
 ;
-DROP TABLE Tracks CASCADE
+DROP TABLE Track CASCADE
 ;
 DROP TABLE Traffic CASCADE
 ;
 DROP TABLE Train CASCADE
+;
+DROP TABLE Workbook CASCADE
+;
+
+CREATE TABLE Brake ( 
+	ID bigint NOT NULL,
+	ID_Train bigint NULL,
+	ID_Carriage bigint NULL
+)
 ;
 
 CREATE TABLE Cargo ( 
@@ -26,22 +37,39 @@ CREATE TABLE Cargo (
 
 CREATE TABLE Carriage ( 
 	ID bigint NOT NULL,
+	ID_Cargo bigint NULL,
+	ID_Track START bigint NULL,
+	ID_Track FINISH bigint NULL,
 	Mass bigint NULL,
-	ID_Track bigint NULL,
-	Traction boolean NULL,
-	ID_Train bigint NULL,
-	ID_Cargo bigint NULL
+	Shutter speed decimal(10,3) NULL,
+	Slow speed decimal(10,3) NULL,
+	Current speed decimal(10,3) NULL,
+	ID_Train bigint NULL
 )
 ;
 
 CREATE TABLE Carriage retarders ( 
 	ID bigint NOT NULL,
+	Type char(255) NULL,
 	ID_Track bigint NULL,
-	Index char(10) NULL
+	Status boolean NULL
 )
 ;
 
-CREATE TABLE Full name ( 
+CREATE TABLE Operation ( 
+	ID bigint NOT NULL,
+	Name char(255) NULL,
+	ID_Train bigint NULL,
+	ID_Switch bigint NULL,
+	ID_Traffic bigint NULL,
+	ID_Carriage retarders bigint NULL,
+	ID_Brake bigint NULL,
+	Lead time timestamp NULL,
+	ID_Track bigint NULL
+)
+;
+
+CREATE TABLE Operator ( 
 	ID bigint NOT NULL,
 	Surname char(100) NULL,
 	Name char(100) NULL,
@@ -49,40 +77,45 @@ CREATE TABLE Full name (
 )
 ;
 
-CREATE TABLE Operator ( 
-	ID bigint NOT NULL,
-	ID_Full Name bigint NULL,
-	Change data timestamp NULL,
-	ID_Train integer NULL
-)
-;
-
 CREATE TABLE Switch ( 
 	ID bigint NOT NULL,
-	ID_Track smallint NULL
+	ID_Track POSSIBLE bigint NULL,
+	ID_Track REAL bigint NULL
 )
 ;
 
-CREATE TABLE Tracks ( 
+CREATE TABLE Track ( 
 	ID bigint NOT NULL,
-	Length decimal(10,2) NULL,
+	Length bigint NULL,
 	Employment boolean NULL
 )
 ;
 
 CREATE TABLE Traffic ( 
 	ID bigint NOT NULL,
-	Value char(7) NULL,
-	ID_Track char(10) NULL
+	ID_Track bigint NULL,
+	Initial value char(10) NULL,
+	Final value char(10) NULL
 )
 ;
 
 CREATE TABLE Train ( 
 	ID bigint NOT NULL,
-	ID_Track bigint NULL,
-	Sending data timestamp NULL,
-	Arrival data timestamp NULL
+	Sending time bigint NULL
 )
+;
+
+CREATE TABLE Workbook ( 
+	ID bigint NOT NULL,
+	ID_Operator bigint NULL,
+	Change data timestamp NULL,
+	ID_Operation integer NULL
+)
+;
+
+
+ALTER TABLE Brake ADD CONSTRAINT PK_Brake 
+	PRIMARY KEY (ID)
 ;
 
 
@@ -91,7 +124,7 @@ ALTER TABLE Cargo ADD CONSTRAINT PK_Cargo
 ;
 
 
-ALTER TABLE Carriage ADD CONSTRAINT PK_Tracks 
+ALTER TABLE Carriage ADD CONSTRAINT PK_Carriage 
 	PRIMARY KEY (ID)
 ;
 
@@ -101,12 +134,12 @@ ALTER TABLE Carriage retarders ADD CONSTRAINT PK_Carriage retarders
 ;
 
 
-ALTER TABLE Full name ADD CONSTRAINT PK_Full name 
+ALTER TABLE Operation ADD CONSTRAINT PK_Operation 
 	PRIMARY KEY (ID)
 ;
 
 
-ALTER TABLE Operator ADD CONSTRAINT PK_Operator 
+ALTER TABLE Operator ADD CONSTRAINT PK_Full name 
 	PRIMARY KEY (ID)
 ;
 
@@ -116,7 +149,7 @@ ALTER TABLE Switch ADD CONSTRAINT PK_Switch
 ;
 
 
-ALTER TABLE Tracks ADD CONSTRAINT PK_Tracks 
+ALTER TABLE Track ADD CONSTRAINT PK_Track 
 	PRIMARY KEY (ID)
 ;
 
@@ -127,5 +160,10 @@ ALTER TABLE Traffic ADD CONSTRAINT PK_Traffic
 
 
 ALTER TABLE Train ADD CONSTRAINT PK_Train 
+	PRIMARY KEY (ID)
+;
+
+
+ALTER TABLE Workbook ADD CONSTRAINT PK_Operator 
 	PRIMARY KEY (ID)
 ;
